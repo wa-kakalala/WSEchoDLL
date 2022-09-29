e98f1b7810e849fd2aa0b083dcba0d6879105117
wangchaosun <2962056732@qq.com>
2022-09-29 14:19:25 +0800

    new structure

---
+++ WSEchoDLL/WSEchoDLL.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#ifdef _DLLAPI
+#define DLLAPI __declspec(dllexport)  // 导出
+#else
+#define DLLAPI __declspec(dllimport)  // 导入
+#endif
+
+#include <stdio.h>
+
+extern "C" DLLAPI int Bind(const char*ip, unsigned short* port);
+extern "C" DLLAPI int Close();
+extern "C" DLLAPI int Receive(void(*f)(const char*msg));
+extern "C" DLLAPI void StopReceive();
+extern "C" DLLAPI int Ping(const char*ip, unsigned short port);
+extern "C" DLLAPI int Init();
+extern "C" DLLAPI void Defer();
